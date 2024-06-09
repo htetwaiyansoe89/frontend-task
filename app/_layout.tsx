7e4644afaae267aeb09967ff197c71392484c349
Htet Wai Yan Soe <johnthelinux@gmail.com>
2024-06-09 10:10:11 +0700

    chore: refactor navigation components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,9 @@ import {Stack} from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import 'react-native-reanimated';
 import {Labels} from "@/constants/Labels";
-import AddCardButton from "@/components/AddCardButton";
+import AddCardButton from "@/components/navigation/AddCardButton";
 import {Colors} from "@/constants/Colors";
-import BackToHomeButton from "@/components/BackToHomeButton";
+import BackToHomeButton from "@/components/navigation/BackToHomeButton";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
